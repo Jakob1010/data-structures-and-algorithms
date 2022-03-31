1b446b04e865ad172ab1e65ce8262630c63cbec0
Jakob1010 <aichinger.j.k@gmail.com>
2022-03-31 20:31:09 +0200

    added unit tests for binary search

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,9 +1,10 @@
 import unittest
-
+from unittest import mock
 
 class BinarySearch:
     # Returns index of target in nums if present, else -1
-    def binary_search(self, nums, target):
+    @staticmethod
+    def binary_search(nums, target):
         n = len(nums)
         l = 0
         r = n - 1
@@ -26,8 +27,18 @@ class TestBinarySearch(unittest.TestCase):
     def test_binary_search(self):
         bs = BinarySearch()
         nums = [1, 2, 3, 4, 5, 10, 12, 14, 345]
+
+        # existing target
         self.assertEqual(bs.binary_search(nums, nums[3]), 3)
+        # index 0
         self.assertEqual(bs.binary_search(nums, nums[0]), 0)
+        # index n-1
         self.assertEqual(bs.binary_search(nums, nums[len(nums)-1]), len(nums)-1)
+        # existing target
         self.assertEqual(bs.binary_search(nums, nums[6]), 6)
+        # non existing target
         self.assertEqual(bs.binary_search(nums, 100), -1)
+
+    if __name__ == '__main__':
+        unittest.main()
+
