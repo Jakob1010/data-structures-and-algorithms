e47a6ce11846c89341df745ab04ca4e908fd8e9d
Jakob <aichinger.j.k@gmail.com>
2022-05-11 18:43:14 +0200

    Time: 1376 ms (88.61%), Space: 40.7 MB (78.36%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,21 +1,8 @@
 class Solution:
-    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        managers = {}
-        num_of_minutes = 0
-        for i,m in enumerate(manager):
-            if m in managers:
-                managers[m].append(i)
-            elif m != -1:
-                managers[m] = [i]
-         
-        def find_max_num_of_minutes(emp):
-            max_minutes = 0
-            if informTime[emp] == 0:
-                return max_minutes
-            else:
-                for sub_emp in managers[emp]:
-                    emp_time = informTime[emp]
-                    max_minutes = max(max_minutes,emp_time + find_max_num_of_minutes(sub_emp))
-            return max_minutes
-        
-        return find_max_num_of_minutes(headID)
+    def numOfMinutes(self, n, headID, manager, informTime):
+        def dfs(i):
+            if manager[i] != -1:
+                informTime[i] += dfs(manager[i])
+                manager[i] = -1
+            return informTime[i]
+        return max(map(dfs, range(n)))
