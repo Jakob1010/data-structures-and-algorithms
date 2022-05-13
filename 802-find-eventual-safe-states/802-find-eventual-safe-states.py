53905d832c4d597ad487f9b5aec67363ee11bc30
Jakob <aichinger.j.k@gmail.com>
2022-05-13 21:27:11 +0200

    Time: 1128 ms (17.81%), Space: 23.3 MB (12.68%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.py
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -1,13 +1,37 @@
 class Solution:
-    def eventualSafeNodes(self, graph):
-        def explore(i):
-            visited[i] = 0
-            for v in graph[i]:
-                if visited[v] == 0 or (visited[v] == -1 and explore(v)): return True
-            visited[i] = 1
-            res.append(i)
-            return False
-        visited, res = [-1] * len(graph), []
-        for i in range(len(graph)):
-            if visited[i] == -1: explore(i)
-        return sorted(res)
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        outgoing = {}
+        incoming = {}
+        top_sort = []
+        terminate_nodes = []
+        
+        for i,edges in enumerate(graph):
+            if len(edges) == 0:
+                terminate_nodes.append(i)
+                top_sort.append(i)
+            else:
+                for edge in edges:
+                    if i in outgoing:
+                        outgoing[i].add(edge)
+                    else:
+                        outgoing[i] = set()
+                        outgoing[i].add(edge)
+
+                    if edge in incoming:
+                        incoming[edge].append(i)
+                    else:
+                        incoming[edge] = [i]
+
+                
+        while top_sort:
+            node = top_sort.pop(0)
+            if node in incoming:
+                for adj_node in incoming[node]:
+                    outgoing[adj_node].remove(node)
+                    if len(outgoing[adj_node]) == 0:
+                        top_sort.append(adj_node)
+                        terminate_nodes.append(adj_node)
+                
+                
+                
+        return sorted(terminate_nodes)
