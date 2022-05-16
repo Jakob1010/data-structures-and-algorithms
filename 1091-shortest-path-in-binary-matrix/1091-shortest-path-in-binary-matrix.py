6d923caa21b3d9fa54ccc31b52fc793db471b661
Jakob <aichinger.j.k@gmail.com>
2022-05-16 08:03:50 +0200

    Time: 968 ms (33.79%), Space: 15.5 MB (26.25%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,31 +1,28 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1:
+        if grid[0][0] != 0 or grid[len(grid)-1][len(grid[0])-1] != 0:
             return -1
-        elif len(grid) == 1 and len(grid[0]) == 1:
+        if len(grid)==1 and len(grid[0])==1:
             return 1
-        
-        shortest_path = 1
+    
         q = deque([(0,0)])
-        r = len(grid)
-        c = len(grid[0])
-        bottom_right = (r-1,c-1)
-        grid[0][0] = 1
+        visited = set()
+        shortest_path = 1
         
+        visited.add((0,0))
         while q:
             n = len(q)
             for i in range(n):
                 x,y = q.popleft()
-                # add 8-directionally adj. fields
                 for new_x, new_y in ((x+1,y),(x-1,y),(x,y+1),(x,y-1),(x+1,y+1),(x-1,y-1),(x+1,y-1),(x-1,y+1)):
-                    if new_x < 0 or new_y < 0 or new_x >= r or new_y >= c or grid[new_x][new_y]==1:
+                    if new_x < 0 or new_y < 0 or new_x >= len(grid) or new_y >= len(grid[0]) or grid[new_x][new_y] == 1 or (new_x,new_y) in visited:
                         continue
-                    elif (new_x,new_y) == bottom_right:
-                        return shortest_path + 1
+                    elif new_x == len(grid)-1 and new_y == len(grid[0])-1:
+                        return shortest_path+1
                     else:
-                        grid[new_x][new_y] = 1
                         q.append((new_x,new_y))
+                        visited.add((new_x,new_y))
             shortest_path += 1
-        
-        
         return -1
+                    
+        
