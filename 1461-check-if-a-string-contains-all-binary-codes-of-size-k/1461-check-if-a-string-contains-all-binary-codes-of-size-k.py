9be5c4f6b9856130d6901b5344fadd6e42eadd03
Jakob <aichinger.j.k@gmail.com>
2022-06-11 11:13:58 +0200

    Time: 916 ms (18.33%), Space: 39.1 MB (10.04%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,17 +1,21 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        found_combinations = set()
-        combinations_cnt = 0
-        all_ones = (1<<k)-1
+        if len(s) < k:
+            return False
         
+        needed = 1 << k
+        all_ones = needed - 1
+        combinations = set()
         current = 0
+
         
         for i in range(0,len(s)):
-            current = ((current << 1) & all_ones) | int(s[i])                      
-            if i>=k-1 and current not in found_combinations:
-                found_combinations.add(current)
-                combinations_cnt += 1
-            if combinations_cnt == all_ones+1:
-                return True        
-            
+            current = ((current << 1) & all_ones) | int(s[i])
+            if i>=k-1 and current not in combinations:
+                combinations.add(current)
+                needed-=1
+                if needed == 0: return True
+                
         return False
+                
+        
