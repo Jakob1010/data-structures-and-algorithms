147b051b39291ab6011901ad20522e475356a08a
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2022-12-21 10:09:42 +0100

    added unionfind

--- 268-missing-number/268-missing-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        missing_num = 0
-        
-        for i,num in enumerate(nums):
-            missing_num += i
-            missing_num -= num
-            
-        return missing_num+len(nums)
